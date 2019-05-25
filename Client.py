3fe7cea63647cc166f3327507ba03ed8119a0e9a
Jordan Chernof <jordan.chernof@gmail.com>
2019-05-25 13:15:52 -0400

    add event filtering

--- Client.py
+++ Client.py
@@ -1,36 +1,37 @@
 from web3 import Web3
 from web3.contract import ConciseContract
+from web3.auto import w3
+import time
 class Client():
 
     def __init__(self, address, host, abi):
-        self.web3 = Web3(Web3.HTTPProvider(host))
+        self.web3 = Web3(Web3.WebsocketProvider(host))
+
         if (self.web3.isConnected()):
             self.contract = self.web3.eth.contract(address=address, abi=abi)
             self.concise = ConciseContract(self.contract)
-            # self.block = self.web3.eth.filter(fil) # should be logfilter
-            self.blockNumber = self.web3.eth.blockNumber
+            self.result = True
+            self.address = address
         else:
             print('Unable to connect with host', host, 'and address', str(address))
             self.result = False
-            return
-        self.result = True
 
-    def getFunctions(self):
+    ''' Prints all functions applicable to Contract object '''
+    def printFunctions(self):
         print(self.contract.all_functions())
 
-    def getCode(self):
-        a = (self.web3.eth.getCode("0xB9280B318A84df5891610F27625b0741951B94B4"))
-        print(a.hex())
-
-    def getResult(self):
+    ''' Prints Connection Status '''
+    def printConnectionResult(self):
         if (self.result):
             print("Connected")
         else:
             print("Not connected")
 
-    def getContract(self):
+    ''' Gets Contract object '''
+    def printContract(self):
         print(self.contract)
 
+    ''' Gets Transaction hash from block and transaction index '''
     def getTx(self, b, i):
         tx = self.web3.eth.getTransactionByBlock(b, i)
         # print("~~~~~~")
@@ -50,9 +51,35 @@ class Client():
         return (tx['blockHash'], tx['hash'])
 
     def getBlockNumber(self):
-        return self.blockNumber
+        return self.web3.eth.blockNumber
 
     def getReceipt(self, tx):
         receipt = self.web3.eth.getTransactionReceipt(tx)
         return receipt['contractAddress']
+
+    def createEventFilter(self):
+        ''' self.contract '''
+        events = self.contract.events
+        filter = self.web3.eth.filter({'address': self.address})
+        print("Successfully created filter",filter)
+
+    def handle_event(event):
+        print(event)
+    def log_loop(event_filter, poll_interval):
+        while True:
+            for event in event_filter.get_new_entries():
+                handle_event(event)
+            time.sleep(poll_interval)    
+
+    def listen(self):
+        events = self.contract.events
+        lst = []
+        transfers = events.Transfer()
+        #print(dir(transfers))
+
+        print('~~Getting events log~~')
+        print('')
+        log_filter = transfers.createFilter(fromBlock=7829800, address=self.address)
         
+        print(log_filter.get_all_entries())
+        # print(events._events)
--- main.py
+++ main.py
@@ -2,48 +2,38 @@ import json
 
 import Client as c
 import Host as h
-
 host = h.Host()
+
 url = host.getUrl()
-contractID = "0xB9280B318A84df5891610F27625b0741951B94B4"
+contractID = "0x6Bc27b0e29D235628F955B6173715bB83D1c4aC5" 
 abi = '[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]' # json array
 
-ans = 7826565
-
-# filters don't work
-
 client = c.Client(contractID, url, abi)
-client.getResult()
-target_contract = contractID
-# client.getTx()
-# client.getBlock(3)
+client.printConnectionResult()
 
-numBlocks = client.getBlockNumber()
 
-# client.getCode()
 
-for b in range(numBlocks):
-    for i in range(client.getNumTransactions(numBlocks - b)):
-        # print("checking transaction",i,"of block",numBlocks-b)
-        if not client.getTx(numBlocks-b, i):
-            print("Found null ~to~ attribute")
-            block_hash, tx_hash = client.getHashes(numBlocks-b, i)
-            if (client.getReceipt(tx_hash) == target_contract):
-                print('Block', block_hash)
-                print('Transaction', tx_hash)
-                break
 
 '''
-for block in blocks:
-    for tx in block.transactions:
-        if tx.contract == contract:
-            print("Block", block.hash)
-            print("Transaction", tx.hash)
-            return
-
-
-Loop through all Blocks
-In each block B, loop through all transactions
-In each transaction, check if contract ID = our contract
-    If true, print B's hash and print transaction's hash
+Lazy method to find correct transaction
+1. Loop through all Blocks
+2. In each block, loop through all transactions
+3. In each transaction, check if the receiver ID is 0
+    a. If true, check if the transaction receipt's contract address matches the contract address we're searching
 '''
+def naiveApproach():
+    numBlocks = client.getBlockNumber()
+    for b in range(numBlocks):
+        for i in range(client.getNumTransactions(numBlocks - b)):
+            # print("checking transaction",i,"of block",numBlocks-b)
+            if not client.getTx(numBlocks-b, i):
+                print("Found null ~to~ attribute")
+                block_hash, tx_hash = client.getHashes(numBlocks-b, i)
+                if (client.getReceipt(tx_hash) == contractID):
+                    print('Block', block_hash)
+                    print('Transaction', tx_hash)
+                    break
+
+client.listen()
+
+
