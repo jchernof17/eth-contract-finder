5bfe065afddcda9f7b315a7c4adcba230e0d971f
Jordan Chernof <jordan.chernof@gmail.com>
2019-05-26 12:40:28 -0400

    Change approach to getCode() and search approach

--- Client.py
+++ Client.py
@@ -1,85 +1,71 @@
 from web3 import Web3
-from web3.contract import ConciseContract
-from web3.auto import w3
-import time
+import Host
 class Client():
 
-    def __init__(self, address, host, abi):
-        self.web3 = Web3(Web3.WebsocketProvider(host))
+    def __init__(self, address, host = Host.url):
+        try:
+            self.web3 = Web3(Web3.HTTPProvider(host))
+        except Exception as e:
+            print("Could not connect with host", host, "and address", str(address))
+            raise e;
 
-        if (self.web3.isConnected()):
-            self.contract = self.web3.eth.contract(address=address, abi=abi)
-            self.concise = ConciseContract(self.contract)
+        if self.web3.isConnected():
             self.result = True
             self.address = address
         else:
             print('Unable to connect with host', host, 'and address', str(address))
             self.result = False
+            exit()
 
-    ''' Prints all functions applicable to Contract object '''
-    def printFunctions(self):
-        print(self.contract.all_functions())
-
-    ''' Prints Connection Status '''
-    def printConnectionResult(self):
-        if (self.result):
-            print("Connected")
-        else:
-            print("Not connected")
-
-    ''' Gets Contract object '''
-    def printContract(self):
-        print(self.contract)
-
-    ''' Gets Transaction hash from block and transaction index '''
-    def getTx(self, b, i):
-        tx = self.web3.eth.getTransactionByBlock(b, i)
-        # print("~~~~~~")
-        # print("Transaction is",tx) # should not be none
-        # print("~~~~~~")
-        tx_contract = tx['to']
-        # print(tx_contract)
-        return tx_contract
-
-    def getNumTransactions(self, b):
-        a = self.web3.eth.getBlockTransactionCount(b)
-        print("Block",b,"has",a,"transactions")
-        return a
-
-    def getHashes(self, b, i):
-        tx = self.web3.eth.getTransactionByBlock(b, i)
-        return (tx['blockHash'], tx['hash'])
-
+    ''' Gets latest block (blockHeight) 
+        returns: an integer block number. '''
     def getBlockNumber(self):
         return self.web3.eth.blockNumber
 
-    def getReceipt(self, tx):
-        receipt = self.web3.eth.getTransactionReceipt(tx)
-        return receipt['contractAddress']
+    '''Checks if block has seen this contract
+        returns: True if contract appears in blockHeight block
+        returns: False otherwise'''
+    def containsContract(self, block):
+        bytecode = self.web3.eth.getCode(self.address, hex(block))
+        return True if bytecode else False
+    
+    '''Helper function to start recursive search
+        returns: an integer block number'''
+    def findFirstContractBlock(self):
+        high = self.getBlockNumber()
+        low = 0
+        mid = (high + low + 1) // 2 # rounds up
+        return self.search(high, low, mid)
 
-    def createEventFilter(self):
-        ''' self.contract '''
-        events = self.contract.events
-        filter = self.web3.eth.filter({'address': self.address})
-        print("Successfully created filter",filter)
+    '''binary searches from blockNumber to find first block that contains
+        the contract whose previous block does not contain the contract.
+        returns: an integer block number.
+        INVARIANTS: 
+        - high ALWAYS contains contract (True)
+        - low NEVER contains contract  (False)'''
+    def search(self, high, low, mid):
+        if high - low == 1:
+            return high
+        elif self.containsContract(mid): # if mid returns True (i.e., is too high)
+            newMid = (mid + low) // 2
+            return self.search(mid, low, newMid)
+        else: # mid is too low
+            newMid = (mid + high) // 2 # if mid returns False (i.e., is too low)
+            return self.search(high, mid, newMid)
 
-    def handle_event(event):
-        print(event)
-    def log_loop(event_filter, poll_interval):
-        while True:
-            for event in event_filter.get_new_entries():
-                handle_event(event)
-            time.sleep(poll_interval)    
+    '''gets block hash from block number and transaction hash that matches contract
+        returns: tuple (hexCode hash of block, hexCode hash of transaction)'''
+    def getHashes(self, b):
+        block = self.web3.eth.getBlock(b)
+        blockHash = block['hash'].hex()
 
-    def listen(self):
-        events = self.contract.events
-        lst = []
-        transfers = events.Transfer()
-        #print(dir(transfers))
+        # find transaction in list of block's transactions
+        assert(block['transactions']) # affirms that there are > 0 txns associated with the block
+        for tx in block['transactions']:
+            txHash = tx.hex()
 
-        print('~~Getting events log~~')
-        print('')
-        log_filter = transfers.createFilter(fromBlock=7829800, address=self.address)
-        
-        print(log_filter.get_all_entries())
-        # print(events._events)
+            # get transaction receipt corresponding to transaction
+            txReceipt = self.web3.eth.getTransactionReceipt(txHash)
+            if (txReceipt['contractAddress'] == self.address):
+                return (blockHash, txHash)
+        return None
