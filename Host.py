6111fc7c5a74ac781cf24d277ec957747ddcce90
Jordan Chernof <jordan.chernof@gmail.com>
2019-05-26 12:41:17 -0400

    Cleanup hosts

--- Host.py
+++ Host.py
@@ -1,10 +1,4 @@
-class Host():
-
-    url = "ENTER_YOUR_HOST_HERE"
-
-    def __init__(self):
-        self.result = True
-        return
-
-    def getUrl(self):
-        return self.url
+# This file is for ease of testing
+# Is not used in main.py file
+url = "https://mainnet.infura.io/v3/dfacd198430a4bc"
+url += "c9f193278b115a433" # make slightly confusing to dissuade github bots from swiping my key (even though it's public)
