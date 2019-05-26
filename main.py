63386e4d28a61eb7ac2649fa61bcea9acb406fc2
Jordan Chernof <jordan.chernof@gmail.com>
2019-05-26 12:41:02 -0400

    Interpret arguments from command line

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-import json
-
+import sys
 import Client as c
-import Host as h
-host = h.Host()
-
-url = host.getUrl()
-contractID = "0x6Bc27b0e29D235628F955B6173715bB83D1c4aC5" 
-abi = '[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]' # json array
-
-client = c.Client(contractID, url, abi)
-client.printConnectionResult()
-
-
-
 
 '''
-Lazy method to find correct transaction
-1. Loop through all Blocks
-2. In each block, loop through all transactions
-3. In each transaction, check if the receiver ID is 0
-    a. If true, check if the transaction receipt's contract address matches the contract address we're searching
+argv => [main.py, contractID, --host, host]
 '''
-def naiveApproach():
-    numBlocks = client.getBlockNumber()
-    for b in range(numBlocks):
-        for i in range(client.getNumTransactions(numBlocks - b)):
-            # print("checking transaction",i,"of block",numBlocks-b)
-            if not client.getTx(numBlocks-b, i):
-                print("Found null ~to~ attribute")
-                block_hash, tx_hash = client.getHashes(numBlocks-b, i)
-                if (client.getReceipt(tx_hash) == contractID):
-                    print('Block', block_hash)
-                    print('Transaction', tx_hash)
-                    break
 
-client.listen()
+# parse input
+if len(sys.argv) != 4:
+    print("Enter transaction and address, e.g.:")
+    print("python main.py 0x... --host https://mainnet.infura.io/v3/[YOUR_ADDRESS_HERE]")
+    exit()
+else:
+    contractID = sys.argv[1]
+    # sets Host.url to the host we were provided 
+    url = sys.argv[3]
 
+# establish connection to server with host and store contract address
+client = c.Client(contractID, url)
+assert(client.result)
+
+firstBlock = client.findFirstContractBlock()
+hashes = client.getHashes(firstBlock)
+
+if not hashes:
+    print("Problem finding transaction.")
+else:
+    print("Block:", hashes[0])
+    print("Transaction:", hashes[1])
 
